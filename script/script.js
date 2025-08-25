c9deb19c9c552c3b2f29f102f16a9d7d6c6f6116
Iury <157545189+IuryPichitelli@users.noreply.github.com>
2025-08-25 20:10:24 -0300

    change

--- script/script.js
+++ script/script.js
@@ -3,10 +3,10 @@ const inputTypePassword = document.getElementById("idPassowordUser")
 
 imgVisibilityOff.addEventListener('click', function(){
     if (imgVisibilityOff.src.endsWith("visibilityOff24px.svg")) {
-        imgVisibilityOff.src = "../images/loginPage/visibilityOn24px.svg";
+        imgVisibilityOff.src = "images/loginPage/visibilityOn24px.svg";
         inputTypePassword.type = "text";
     } else {
-        imgVisibilityOff.src = "../images/loginPage/visibilityOff24px.svg";
+        imgVisibilityOff.src = "images/loginPage/visibilityOff24px.svg";
         inputTypePassword.type = "password";
     }
 
