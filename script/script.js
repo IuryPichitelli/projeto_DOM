3d02d4a7ad0f378b25f257acefe5aece5cfacb84
Iury <157545189+IuryPichitelli@users.noreply.github.com>
2025-08-25 19:49:53 -0300

    ajustando o  login

--- script/script.js
+++ script/script.js
@@ -1,10 +1,13 @@
 const imgVisibilityOff  = document.getElementById("imageLoginEyeVisibility")
+const inputTypePassword = document.getElementById("idPassowordUser")
 
 imgVisibilityOff.addEventListener('click', function(){
     if (imgVisibilityOff.src.endsWith("visibilityOff24px.svg")) {
         imgVisibilityOff.src = "../images/loginPage/visibilityOn24px.svg";
+        inputTypePassword.type = "text";
     } else {
         imgVisibilityOff.src = "../images/loginPage/visibilityOff24px.svg";
+        inputTypePassword.type = "password";
     }
 
 });
